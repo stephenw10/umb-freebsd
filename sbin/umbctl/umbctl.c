65f05daef47909d5789289479feeda8afb9d3a53
Pierre Pronchery <khorben@defora.org>
2018-07-21 21:19:22 +0200

    Complete rewrite of umbctl(8)
    This is meant to be more consistent with existing programs, such as
    pppoectl(8).

--- sbin/umbctl/umbctl.c
+++ sbin/umbctl/umbctl.c
@@ -30,6 +30,7 @@
 #include <sys/ioctl.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
@@ -40,7 +41,7 @@
 
 
 
-
+/* constants */
 static const struct umb_valdescr _umb_regstate[] =
 	MBIM_REGSTATE_DESCRIPTIONS;
 
@@ -72,20 +73,25 @@ static const struct umb_valdescr _umb_ber[] =
 };
 
 
-static int _char_to_utf16(const char *in, uint16_t *out, size_t outlen);
-static int _error(int ret, char const * message);
-static int _print_info(char const * ifname, struct umb_parameter * mp);
-static char const * _print_stars(size_t count);
-static int _umbctl(char const * ifname, char const * apn,
-		char const * username, char const * password, int is_puk,
-		char const * pin);
-static int _umbctl_info(char const * ifname);
+/* prototypes */
+static int _char_to_utf16(const char * in, uint16_t * out, size_t outlen);
+static int _error(int ret, char const * format, ...);
+static int _umbctl(char const * ifname, int verbose, int argc, char * argv[]);
+static int _umbctl_file(char const * ifname, char const * filename, int verbose,
+		int argc, char * argv[]);
+static void _umbctl_info(char const * ifname, struct umb_info * umbi);
+static int _umbctl_ioctl(char const * ifname, int fd, unsigned long request,
+		struct ifreq * ifr);
+static int _umbctl_set(char const * ifname, struct umb_parameter * umbp,
+		int argc, char * argv[]);
 static int _umbctl_socket(void);
 static int _usage(void);
 static void _utf16_to_char(uint16_t *in, int inlen, char *out, size_t outlen);
 
+
+/* functions */
 /* this function is from OpenBSD's ifconfig(8) */
-static int _char_to_utf16(const char *in, uint16_t *out, size_t outlen)
+static int _char_to_utf16(const char * in, uint16_t * out, size_t outlen)
 {
 	int	n = 0;
 	uint16_t c;
@@ -110,104 +116,98 @@ static int _char_to_utf16(const char *in, uint16_t *out, size_t outlen)
 	}
 }
 
-static int _error(int ret, char const * message)
+
+static int _error(int ret, char const * format, ...)
 {
+	va_list ap;
+
 	fputs("umbctl: ", stderr);
-	perror(message);
+	va_start(ap, format);
+	vfprintf(stderr, format, ap);
+	va_end(ap);
+	fputs("\n", stderr);
 	return ret;
 }
 
-static int _print_info(char const * ifname, struct umb_parameter * mp)
-{
-	printf("%s: %s %s, roaming %d, classes %u\n", ifname,
-			mp->is_puk ? "PUK" : "PIN",
-			_print_stars(mp->pinlen), mp->roaming,
-			mp->preferredclasses);
-	return 0;
-}
 
-static char const * _print_stars(size_t count)
-{
-	static char buf[MBIM_PIN_MAXLEN + 1];
-	size_t i;
-
-	for(i = 0; i < count && i < sizeof(buf) - 1; i++)
-		buf[i] = '*';
-	buf[i] = '\0';
-	return buf;
-}
-
-static int _umbctl(char const * ifname, char const * apn,
-		char const * username, char const * password, int is_puk,
-		char const * pin)
+static int _umbctl(char const * ifname, int verbose, int argc, char * argv[])
 {
 	int fd;
 	struct ifreq ifr;
-	struct umb_parameter mp;
+	struct umb_info umbi;
+	struct umb_parameter umbp;
 
 	if((fd = _umbctl_socket()) < 0)
 		return 2;
 	memset(&ifr, 0, sizeof(ifr));
 	strlcpy(ifr.ifr_name, ifname, sizeof(ifr.ifr_name));
-	memset(&mp, 0, sizeof(mp));
-	ifr.ifr_data = &mp;
-	if(ioctl(fd, SIOCGUMBPARAM, &ifr) != 0)
-		return (errno == ENXIO)
-			? _error(2, ifname) : _error(2, "SIOCGUMBPARAM");
-	if(apn == NULL && username == NULL && password == NULL && is_puk < 0)
-		_print_info(ifname, &mp);
-	else
+	if(argc != 0)
 	{
-		if(apn != NULL && (mp.apnlen = _char_to_utf16(apn, mp.apn,
-						sizeof(mp.apn))) < 0)
+		memset(&umbp, 0, sizeof(umbp));
+		ifr.ifr_data = &umbp;
+		if(_umbctl_ioctl(ifname, fd, SIOCGUMBPARAM, &ifr) != 0
+				|| _umbctl_set(ifname, &umbp, argc, argv) != 0
+				|| _umbctl_ioctl(ifname, fd, SIOCSUMBPARAM,
+					&ifr) != 0)
 		{
-			fprintf(stderr, "%s: %s: APN too long\n", "umbctl",
-					apn);
+			close(fd);
+			return 2;
+		}
+	}
+	if(argc == 0 || verbose > 0)
+	{
+		ifr.ifr_data = &umbi;
+		if(_umbctl_ioctl(ifname, fd, SIOCGUMBINFO, &ifr) != 0)
+		{
+			close(fd);
 			return 3;
 		}
-		if(username != NULL && (mp.usernamelen = _char_to_utf16(
-						username, mp.username,
-						sizeof(mp.username))) < 0)
-		{
-			fprintf(stderr, "%s: %s: username too long\n", "umbctl",
-					username);
-			return 3;
-		}
-		if(password != NULL && (mp.passwordlen = _char_to_utf16(
-						password, mp.password,
-						sizeof(mp.password))) < 0)
-		{
-			fprintf(stderr, "%s: %s: password too long\n", "umbctl",
-					_print_stars(strlen(password)));
-			return 3;
-		}
-		if(is_puk >= 0)
-		{
-			mp.is_puk = is_puk;
-			mp.op = MBIM_PIN_OP_ENTER;
-			if((mp.pinlen = _char_to_utf16(pin, mp.pin,
-							sizeof(mp.pin))) < 0)
-			{
-				fprintf(stderr, "%s: %s: %s code too long\n",
-						"umbctl",
-						is_puk ? "PIN" : "PUK",
-						_print_stars(strlen(pin)));
-				return 4;
-			}
-		}
-		if(ioctl(fd, SIOCSUMBPARAM, &ifr) != 0)
-			return _error(2, ifname);
+		_umbctl_info(ifname, &umbi);
 	}
 	if(close(fd) != 0)
-		return _error(2, "close");
+		return _error(2, "%s: %s", ifname, strerror(errno));
 	return 0;
 }
 
-static int _umbctl_info(char const * ifname)
+
+static int _umbctl_file(char const * ifname, char const * filename, int verbose,
+		int argc, char * argv[])
 {
-	int fd;
-	struct ifreq ifr;
-	struct umb_info mi;
+	FILE * fp;
+	struct umb_parameter umbp;
+	char buf[512];
+	int eof;
+	char * tokens[3];
+	char * p;
+	char * last;
+	size_t i;
+
+	if((fp = fopen(filename, "r")) == NULL)
+		return _error(2, "%s: %s", filename, strerror(errno));
+	memset(&umbp, 0, sizeof(umbp));
+	while(fgets(buf, sizeof(buf), fp) != NULL)
+	{
+		if(buf[0] == '#')
+			continue;
+		buf[sizeof(buf) - 1] = '\0';
+		for(i = 0, p = strtok_r(buf, " ", &last); i < 3 && p != NULL;
+				i++, p = strtok_r(NULL, " ", &last))
+			tokens[i] = p;
+		tokens[i] = NULL;
+		if(_umbctl_set(ifname, &umbp, i, tokens) != 0)
+			break;
+	}
+	eof = feof(fp);
+	if(fclose(fp) != 0 || !eof)
+		return _error(2, "%s: %s", filename, strerror(errno));
+	/* FIXME really apply the parameters */
+	_umbctl(ifname, verbose, argc, argv);
+	return 0;
+}
+
+
+static void _umbctl_info(char const * ifname, struct umb_info * umbi)
+{
 	char provider[UMB_PROVIDERNAME_MAXLEN + 1];
 	char pn[UMB_PHONENR_MAXLEN + 1];
 	char roaming[UMB_ROAMINGTEXT_MAXLEN + 1];
@@ -215,56 +215,126 @@ static int _umbctl_info(char const * ifname)
 	char fwinfo[UMB_FWINFO_MAXLEN + 1];
 	char hwinfo[UMB_HWINFO_MAXLEN + 1];
 
-	if((fd = _umbctl_socket()) < 0)
-		return 2;
-	memset(&ifr, 0, sizeof(ifr));
-	strlcpy(ifr.ifr_name, ifname, sizeof(ifr.ifr_name));
-	memset(&mi, 0, sizeof(mi));
-	ifr.ifr_data = &mi;
-	if(ioctl(fd, SIOCGUMBINFO, &ifr) != 0)
-		return (errno == ENXIO)
-			? _error(2, ifname) : _error(2, "SIOCGUMBINFO");
-	_utf16_to_char(mi.provider, UMB_PROVIDERNAME_MAXLEN,
+	_utf16_to_char(umbi->provider, UMB_PROVIDERNAME_MAXLEN,
 			provider, sizeof(provider));
-	_utf16_to_char(mi.pn, UMB_PHONENR_MAXLEN, pn, sizeof(pn));
-	_utf16_to_char(mi.roamingtxt, UMB_ROAMINGTEXT_MAXLEN,
+	_utf16_to_char(umbi->pn, UMB_PHONENR_MAXLEN, pn, sizeof(pn));
+	_utf16_to_char(umbi->roamingtxt, UMB_ROAMINGTEXT_MAXLEN,
 			roaming, sizeof(roaming));
-	_utf16_to_char(mi.apn, UMB_APN_MAXLEN, apn, sizeof(apn));
-	_utf16_to_char(mi.fwinfo, UMB_FWINFO_MAXLEN, fwinfo, sizeof(fwinfo));
-	_utf16_to_char(mi.hwinfo, UMB_HWINFO_MAXLEN, hwinfo, sizeof(hwinfo));
+	_utf16_to_char(umbi->apn, UMB_APN_MAXLEN, apn, sizeof(apn));
+	_utf16_to_char(umbi->fwinfo, UMB_FWINFO_MAXLEN, fwinfo, sizeof(fwinfo));
+	_utf16_to_char(umbi->hwinfo, UMB_HWINFO_MAXLEN, hwinfo, sizeof(hwinfo));
 	printf("%s: state %s, mode %s, registration %s\n"
 			"\tprovider \"%s\", dataclass %s, signal %s\n"
 			"\tphone number \"%s\", roaming \"%s\" (%s)\n"
 			"\tAPN \"%s\", TX %" PRIu64 ", RX %" PRIu64 "\n"
 			"\tfirmware \"%s\", hardware \"%s\"\n",
-			ifname, umb_val2descr(_umb_state, mi.state),
-			umb_val2descr(_umb_regmode, mi.regmode),
-			umb_val2descr(_umb_regstate, mi.regstate),
-			provider, umb_val2descr(_umb_dataclass, mi.cellclass),
-			umb_val2descr(_umb_ber, mi.ber), pn,
-			roaming, mi.enable_roaming ? "allowed" : "denied",
-			apn, mi.uplink_speed, mi.downlink_speed,
+			ifname, umb_val2descr(_umb_state, umbi->state),
+			umb_val2descr(_umb_regmode, umbi->regmode),
+			umb_val2descr(_umb_regstate, umbi->regstate), provider,
+			umb_val2descr(_umb_dataclass, umbi->cellclass),
+			umb_val2descr(_umb_ber, umbi->ber), pn, roaming,
+			umbi->enable_roaming ? "allowed" : "denied",
+			apn, umbi->uplink_speed, umbi->downlink_speed,
 			fwinfo, hwinfo);
+}
+
+
+static int _umbctl_ioctl(char const * ifname, int fd, unsigned long request,
+		struct ifreq * ifr)
+{
+	if(ioctl(fd, request, ifr) != 0)
+		return _error(-1, "%s: %s", ifname, strerror(errno));
 	return 0;
 }
+
+
+static int _umbctl_set(char const * ifname, struct umb_parameter * umbp,
+		int argc, char * argv[])
+{
+	int i;
+
+	for(i = 0; i < argc; i++)
+	{
+		if(strcmp(argv[i], "apn") == 0 && i + 1 < argc)
+		{
+			umbp->apnlen = _char_to_utf16(argv[i + 1],
+					umbp->apn, sizeof(umbp->apn));
+			if(umbp->apnlen < 0 || (size_t)umbp->apnlen
+					> sizeof(umbp->apn))
+				return _error(-1, "%s: %s", ifname,
+						"APN too long");
+			i++;
+		}
+		else if(strcmp(argv[i], "username") == 0 && i + 1 < argc)
+		{
+			umbp->usernamelen = _char_to_utf16(argv[i + 1],
+					umbp->username, sizeof(umbp->username));
+			if(umbp->usernamelen < 0 || (size_t)umbp->usernamelen
+					> sizeof(umbp->username))
+				return _error(-1, "%s: %s", ifname,
+						"Username too long");
+			i++;
+		}
+		else if(strcmp(argv[i], "password") == 0 && i + 1 < argc)
+		{
+			umbp->passwordlen = _char_to_utf16(argv[i + 1],
+					umbp->password, sizeof(umbp->password));
+			if(umbp->passwordlen < 0 || (size_t)umbp->passwordlen
+					> sizeof(umbp->password))
+				return _error(-1, "%s: %s", ifname,
+						"Password too long");
+			i++;
+		}
+		else if(strcmp(argv[i], "pin") == 0 && i + 1 < argc)
+		{
+			umbp->is_puk = 0;
+			umbp->op = MBIM_PIN_OP_ENTER;
+			umbp->pinlen = _char_to_utf16(argv[i + 1], umbp->pin,
+					sizeof(umbp->pin));
+			if(umbp->pinlen < 0 || (size_t)umbp->pinlen
+					> sizeof(umbp->pin))
+				return _error(-1, "%s: %s", ifname,
+						"PUK code too long");
+			i++;
+		}
+		else if(strcmp(argv[i], "puk") == 0 && i + 1 < argc)
+		{
+			umbp->is_puk = 1;
+			umbp->op = MBIM_PIN_OP_ENTER;
+			umbp->pinlen = _char_to_utf16(argv[i + 1], umbp->pin,
+					sizeof(umbp->pin));
+			if(umbp->pinlen < 0 || (size_t)umbp->pinlen
+					> sizeof(umbp->pin))
+				return _error(-1, "%s: %s", ifname,
+						"PIN code too long");
+			i++;
+		}
+		else
+			return _error(-1, "%s: Unknown or incomplete parameter",
+					argv[i]);
+	}
+	return 0;
+}
+
 
 static int _umbctl_socket(void)
 {
 	int fd;
 
 	if((fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
-		return _error(-1, "socket");
+		return _error(-1, "socket: %s", strerror(errno));
 	return fd;
 }
 
+
 static int _usage(void)
 {
-	fputs("Usage: umbctl interface\n"
-"       umbctl -I interface\n"
-"       umbctl -a apn|-u username|-p password|-s PIN|-S PUK interface\n",
+	fputs("Usage: umbctl [-v] ifname [parameter[=value]] [...]\n"
+"       umbctl -f config-file ifname [...]\n",
 			stderr);
 	return 1;
 }
+
 
 static void _utf16_to_char(uint16_t *in, int inlen, char *out, size_t outlen)
 {
@@ -283,45 +353,30 @@ static void _utf16_to_char(uint16_t *in, int inlen, char *out, size_t outlen)
 	}
 }
 
+
 int main(int argc, char * argv[])
 {
 	int o;
-	int info = 0;
-	char const * apn = NULL;
-	char const * username = NULL;
-	char const * password = NULL;
-	int is_puk = -1;
-	char const * pin = NULL;
+	char const * filename = NULL;
+	int verbose = 0;
 
-	while((o = getopt(argc, argv, "a:Ip:S:s:u:")) != -1)
+	while((o = getopt(argc, argv, "f:v")) != -1)
 		switch(o)
 		{
-			case 'a':
-				apn = optarg;
+			case 'f':
+				filename = optarg;
 				break;
-			case 'I':
-				info = 1;
-				break;
-			case 'p':
-				password = optarg;
-				break;
-			case 's':
-				pin = optarg;
-				is_puk = 0;
-				break;
-			case 'S':
-				pin = optarg;
-				is_puk = 1;
-				break;
-			case 'u':
-				username = optarg;
+			case 'v':
+				verbose++;
 				break;
 			default:
 				return _usage();
 		}
-	if(optind + 1 != argc)
+	if(optind == argc)
 		return _usage();
-	return (info != 0)
-		? _umbctl_info(argv[optind])
-		: _umbctl(argv[optind], apn, username, password, is_puk, pin);
+	if(filename != NULL)
+		return _umbctl_file(argv[optind], filename, verbose,
+				argc - optind - 1, &argv[optind + 1]);
+	return _umbctl(argv[optind], verbose, argc - optind - 1,
+			&argv[optind + 1]);
 }
